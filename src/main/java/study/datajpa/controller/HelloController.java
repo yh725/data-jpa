2ca7920f25bea7fbd236a8ac3c70d38d7ec20d98
yh725 <yh725@kakao.com>
2022-12-20 07:24:41 +0900

    프로젝트 환경설정 - 라이브러리 살펴보기 - H2 데이터베이스 설치 - 스프링 데이터 JPA와 DB 설정, 동작확인

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -1,4 +1,4 @@
-package study.datajpa;
+package study.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
