5db0bf87424eb3d6136b08fedc7ceb5dc21a5a7f
yh725 <yh725@kakao.com>
2022-12-23 22:43:05 +0900

    쿼리 메소드 기능 - 메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,7 +10,6 @@ import study.datajpa.entity.Member;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -60,5 +59,25 @@ class MemberRepositoryTest {
 		memberRepository.delete(member2);
 		long deleteCount = memberRepository.count();
 		assertThat(deleteCount).isEqualTo(0);
+	}
+
+	@Test
+	public void findByUsernameAndAgeGreaterThen() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+		List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+		assertThat(result.get(0).getAge()).isEqualTo(20);
+		assertThat(result.size()).isEqualTo(1);
+	}
+
+	@Test
+	public void findHelloBy() {
+		List<Member> helloBy = memberRepository.findTop3HelloBy();
 	}
 }
