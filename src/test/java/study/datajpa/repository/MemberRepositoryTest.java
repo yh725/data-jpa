33dad38373f092fee16f1d8c262b13ba25badcee
yh725 <yh725@kakao.com>
2022-12-26 22:05:17 +0900

    쿼리 메소드 기능 - 반환 타입 - 순수 JPA 페이징과 정렬 - 스프링 데이터 JPA 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -11,6 +15,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,8 +24,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Rollback(false)
 class MemberRepositoryTest {
 
-	@Autowired MemberRepository memberRepository;
-	@Autowired TeamRepository teamRepository;
+	@Autowired
+	MemberRepository memberRepository;
+	@Autowired
+	TeamRepository teamRepository;
 
 	@Test
 	public void testMember() {
@@ -155,5 +162,105 @@ class MemberRepositoryTest {
 		for (Member member : result) {
 			System.out.println("member = " + member);
 		}
-		}
 	}
+
+	@Test
+	public void returnType() {
+
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("BBB", 20);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+//		List<Member> result = memberRepository.findByUsername("AAAB");
+//		System.out.println("result = " + result.size());
+
+//		Member findMember = memberRepository.findMemberByUsername("AAA");
+//		System.out.println("findMember = " + findMember);
+
+//		Optional<Member> aaa = memberRepository.findOptionalByUsername("AAA");
+//		System.out.println("aaa = " + aaa);
+	}
+
+	@Test
+	public void paging() {
+		//given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 10));
+		memberRepository.save(new Member("member3", 10));
+		memberRepository.save(new Member("member4", 10));
+		memberRepository.save(new Member("member5", 10));
+		memberRepository.save(new Member("member6", 10));
+		memberRepository.save(new Member("member7", 10));
+
+		int age = 10;
+		PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+		//when
+
+		Page<Member> page = memberRepository.findByAge(age, pageRequest);
+
+		Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+
+		//페이지 계산공식 적용
+		//totalPage = totalCount / size
+		//마지막 페이지
+		//최초 페이지
+
+		//then
+		List<Member> content = page.getContent();
+		long totalElements = page.getTotalElements();
+
+		for (Member member : content) {
+			System.out.println("member = " + member);
+		}
+		System.out.println("totalElements = " + totalElements);
+
+		assertThat(content.size()).isEqualTo(3);
+		assertThat(page.getTotalElements()).isEqualTo(7);
+		assertThat(page.getNumber()).isEqualTo(0);
+		assertThat(page.getTotalPages()).isEqualTo(3);
+		assertThat(page.isFirst()).isTrue();
+		assertThat(page.hasNext()).isTrue();
+	}
+
+	@Test
+	public void slice() {
+		//given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 10));
+		memberRepository.save(new Member("member3", 10));
+		memberRepository.save(new Member("member4", 10));
+		memberRepository.save(new Member("member5", 10));
+		memberRepository.save(new Member("member6", 10));
+		memberRepository.save(new Member("member7", 10));
+
+		int age = 10;
+		PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+		//when
+
+		Slice<Member> page = memberRepository.findSliceByAge(age, pageRequest);
+
+		//페이지 계산공식 적용
+		//totalPage = totalCount / size
+		//마지막 페이지
+		//최초 페이지
+
+		//then
+		List<Member> content = page.getContent();
+//		long totalElements = page.getTotalElements();
+
+		for (Member member : content) {
+			System.out.println("member = " + member);
+		}
+//		System.out.println("totalElements = " + totalElements);
+
+		assertThat(content.size()).isEqualTo(3);
+//		assertThat(page.getTotalElements()).isEqualTo(7);
+		assertThat(page.getNumber()).isEqualTo(0);
+//		assertThat(page.getTotalPages()).isEqualTo(3);
+		assertThat(page.isFirst()).isTrue();
+		assertThat(page.hasNext()).isTrue();
+	}
+}
