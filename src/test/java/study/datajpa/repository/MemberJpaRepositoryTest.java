a5a7791ed62e9f6b033eacc591b1d29aa66038cc
yh725 <yh725@kakao.com>
2022-12-27 08:00:36 +0900

    쿼리 메소드 기능 - 벌크성 수정 쿼리 - @EntityGraph - JPA Hint & Lock

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -120,4 +120,20 @@ class MemberJpaRepositoryTest {
 		assertThat(members.size()).isEqualTo(3);
 		assertThat(totalCount).isEqualTo(7);
 	}
+
+	@Test
+	public void bulkUpdate() {
+		//given
+		memberJpaRepository.save(new Member("member1", 10));
+		memberJpaRepository.save(new Member("member2", 19));
+		memberJpaRepository.save(new Member("member3", 20));
+		memberJpaRepository.save(new Member("member4", 21));
+		memberJpaRepository.save(new Member("member5", 40));
+
+		//when
+		int resultCunt = memberJpaRepository.bulkAgePlus(20);
+
+		//then
+		assertThat(resultCunt).isEqualTo(3);
+	}
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,8 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +30,8 @@ class MemberRepositoryTest {
 	MemberRepository memberRepository;
 	@Autowired
 	TeamRepository teamRepository;
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	public void testMember() {
@@ -262,5 +266,90 @@ class MemberRepositoryTest {
 //		assertThat(page.getTotalPages()).isEqualTo(3);
 		assertThat(page.isFirst()).isTrue();
 		assertThat(page.hasNext()).isTrue();
+	}
+
+	@Test
+	public void bulkUpdate() {
+		//given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 19));
+		memberRepository.save(new Member("member3", 20));
+		memberRepository.save(new Member("member4", 21));
+		memberRepository.save(new Member("member5", 40));
+
+		//when
+		int resultCunt = memberRepository.bulkAgePlus(20);
+//		em.flush();
+//		em.clear();
+
+		List<Member> result = memberRepository.findByUsername("member5");
+		Member member5 = result.get(0);
+		System.out.println("member5 = " + member5);
+
+		//then
+		assertThat(resultCunt).isEqualTo(3);
+	}
+
+	@Test
+	public void findMemberLazy() throws Exception {
+	    //given
+	    //member1 -> teamA
+		//member2 -> teamB
+
+		Team teamA = new Team("teatA");
+		Team teamB = new Team("teatB");
+		teamRepository.save(teamA);
+		teamRepository.save(teamB);
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 10, teamB);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		em.flush();
+		em.clear();
+
+		//when
+		//select Member 1 + N
+		List<Member> members = memberRepository.findAll();
+
+		for (Member member : members) {
+			System.out.println("member = " + member);
+			System.out.println("member.teamClass = " + member.getTeam().getClass());
+			System.out.println("member.team = " + member.getTeam().getName());
+		}
+
+		//then
+
+	}
+
+	@Test
+	public void queryHint() {
+		//given
+		Member member1 = memberRepository.save(new Member("member1", 10));
+		em.flush();
+		em.clear();
+
+		//when
+//		Member findMember = memberRepository.findById(member1.getId()).get();
+//		findMember.setUsername("member2");
+
+		Member findMember = memberRepository.findReadOnlyByUsername("member1");
+		findMember.setUsername("member2");
+
+		em.flush();
+	}
+
+	@Test
+	public void lock() {
+		//given
+		Member member1 = memberRepository.save(new Member("member1", 10));
+		em.flush();
+		em.clear();
+
+		//when
+//		Member findMember = memberRepository.findById(member1.getId()).get();
+//		findMember.setUsername("member2");
+
+		memberRepository.findLockByUsername("member1");
 	}
 }
